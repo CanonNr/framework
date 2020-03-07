97b177e3cf56261d88a4f3aecd5d70e799c84715
YR sun <717437709@qq.com>
2020-03-08 02:24:02 +0800

    app文件

--- src/Application.php
+++ src/Application.php
@@ -1,6 +1,30 @@
 <?php
 namespace Canon\Framework;
 
-class Application{
+use Illuminate\Container\Container;
 
+class Application extends Container
+{
+
+    /**
+     * @var string
+     */
+    private $version = "开发中";
+
+
+    public function __construct()
+    {
+        $container = Container::getInstance();
+        $container->bind(Animal::class);
+
+    }
+
+    /**
+     * 获取版本号
+     * @return mixed
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
 }
